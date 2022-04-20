33da10705dd80a7691931e72baa536a35518fa28
PTRL481\Anusha.Medichelmi <anusha.m@xcubelabs.com>
2022-04-20 06:59:30 +0530

    Spawing enemies is done.Enemy controller script is added.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float rotationSpeed;
+     SpawnManager spawnManager;
+    public Transform bulletLaunch;
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        spawnManager = GetComponent<SpawnManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        transform.rotation = transform.rotation * Quaternion.Euler(0, mouseX * rotationSpeed, 0);
+        Camera cam = GetComponentInChildren<Camera>();
+        cam.transform.localRotation = Quaternion.Euler(-mouseY, 0, 0) * cam.transform.localRotation;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            HitEnemy();
+        }
+    }
+
+    public void HitEnemy()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(bulletLaunch.position, bulletLaunch.forward, out hit, 100f))
+        {
+            GameObject hitEnemy = hit.collider.gameObject;
+            if (hitEnemy.gameObject.tag == "Enemy")
+            {
+                //Destroy(hitEnemy);
+                print("Enemy hit");
+                hitEnemy.GetComponent<EnemyController>().EnemyDead();
+            }
+        }
+
+
+
+    }
+
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject enemyPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Vector3 enemyPosition = new Vector3(Random.Range(-10, 11), 5, Random.Range(10, 11));
+            Instantiate(enemyPrefab, enemyPosition, Quaternion.identity);
+        }
+    }
+}
