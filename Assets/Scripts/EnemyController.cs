e4c3adeddf47088f0b259d90b65ab62bd7e9f045
PTRL481\Anusha.Medichelmi <anusha.m@xcubelabs.com>
2022-04-20 06:58:20 +0530

    PlayerMovwment is done

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    NavMeshAgent agent;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EnemyDead()
+    {
+        Debug.Log("Enemy is dead");
+       Destroy(gameObject);
+    }
+}
